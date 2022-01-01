7de5e447a0fd276d65ecdb984485efac3d5485bc
desirel <two_somang@icloud.com>
2022-01-01 12:04:42 +0900

    Initial commit

---
+++ SwiftUI_Image_List/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUI_Image_List
+//
+//  Created by Desire L on 2022/01/01.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Image_List/SwiftUI_Image_ListApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUI_Image_ListApp.swift
+//  SwiftUI_Image_List
+//
+//  Created by Desire L on 2022/01/01.
+//
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Image_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
